582744bd9f70fe78bf444076a0ffa4dc60e5fc8a
Danelia Sanchez <danelia_sanchez20@hotmail.com>
2021-02-21 19:26:41 -0600

    add useRef hook

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useReducer, useMemo } from 'react';
+import { useState, useEffect, useReducer, useMemo, useRef } from 'react';
 import '../styles/Characters.css';
 
 const initialState = {
@@ -21,6 +21,7 @@ const Characters = () => {
     const [characters, setCharacters] = useState([]);
     const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
     const [search,setSearch] = useState('');
+    const searchInput = useRef(null);
 
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character/')
@@ -32,8 +33,8 @@ const Characters = () => {
         dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite });
     }
 
-    const handleSearch = event => {
-        setSearch(event.target.value);
+    const handleSearch = () => {
+        setSearch(searchInput.current.value);
     }
 
     // const filteredUsers = characters.filter((user) => {
@@ -50,7 +51,7 @@ const Characters = () => {
         <div className='Characters'>
             
             <div className="Search">
-                <input type="text" value={search} onChange ={handleSearch} />
+                <input type="text" value={search} ref={searchInput} onChange ={handleSearch} />
             </div>
 
             <div className="Favorites">
